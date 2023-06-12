c8e593ae49ed148d9594143bf566a3d4f5d8f341
Dao Dang Quang <dquang.haiphong@gmail.com>
2023-06-12 18:38:11 +0700

    Đồ án Web Tĩnh

--- js/main.js
+++ js/main.js
@@ -13,3 +13,20 @@ function testimonialSlider() {
   }
 }
 testimonialSlider();
+/**
+ * course preview video
+ */
+function coursePreviewVideo() {
+  const coursePreviewModal = document.querySelector(".js-course-preview-modal");
+  if (coursePreviewModal) {
+    // If the element exists
+    coursePreviewModal.addEventListener("shown.bs.modal", function () {
+      this.querySelector(".js-course-preview-video").play();
+      this.querySelector(".js-course-preview-video").currentTime = 0;
+    });
+    coursePreviewModal.addEventListener("hide.bs.modal", function () {
+      this.querySelector(".js-course-preview-video").pause();
+    });
+  }
+}
+coursePreviewVideo();
